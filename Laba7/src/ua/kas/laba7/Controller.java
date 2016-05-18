b7e2549624eb1a949096fda74e6840cf315fea76
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-05-18 13:58:11 +0300

    ///

--- Laba7/src/ua/kas/laba7/Controller.java
+++ Laba7/src/ua/kas/laba7/Controller.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Random;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
--- Laba7/src/ua/kas/laba7/Thread_constr.java
+++ Laba7/src/ua/kas/laba7/Thread_constr.java
@@ -1,8 +1,6 @@
 package ua.kas.laba7;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
